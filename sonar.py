c96d13400ae2ba44798351a983234c5e04804f16
aoricci6 <166945719+aoricci6@users.noreply.github.com>
2024-07-05 02:09:57 -0300

    Update sonar.py

--- sonar.py
+++ sonar.py
@@ -5,5 +5,5 @@ import time
 while True:
     numero_aleatorio = random.randint(1, 100)  # Genera un nÃºmero aleatorio entre 1 y 100
     print(f"NÃºmero aleatorio: {numero_aleatorio}")
-   # print
+    print
     time.sleep(10)  # Pausa la ejecuciÃ³n durante 10 segundos
